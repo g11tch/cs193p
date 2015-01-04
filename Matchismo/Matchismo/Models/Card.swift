9f62d97c51b660f3231efeee931d1af24df1fdb2
Jake Craige <jake@poeticsystems.com>
2015-01-04 11:45:54 -0600

    add card model

---
+++ Matchismo/Matchismo/Models/Card.swift
@@ -0,0 +1,28 @@
+//
+//  Card.swift
+//  Matchismo
+//
+//  Created by Jake Craige on 1/4/15.
+//  Copyright (c) 2015 Jake Craige. All rights reserved.
+//
+
+import Foundation
+
+class Card : NSObject {
+    var contents = ""
+    var chosen = false
+    var matched = false
+    
+    func isChosen() -> Bool { return self.chosen; }
+    func isMatched() -> Bool { return self.matched; }
+    
+    func match(otherCards: [Card]) -> Int {
+        return otherCards.reduce(0, { score, card in
+            if card.contents == self.contents {
+                return score + 1
+            } else {
+                return score
+            }
+        })
+    }
+}
