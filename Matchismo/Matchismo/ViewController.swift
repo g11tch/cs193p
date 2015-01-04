26df3bff91920282db0c509e09e7398c4dcf569d
Jake Craige <jake@poeticsystems.com>
2015-01-04 11:13:03 -0600

    implement live demo example from course in swift

--- Matchismo/Matchismo/ViewController.swift
+++ Matchismo/Matchismo/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var flipLabel: UILabel!
+    var flipCount : Int = 0 {
+        didSet {
+            self.updateFlipLabel()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +26,37 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func updateFlipLabel() {
+        self.flipLabel.text = "Flips: \(self.flipCount)"
+    }
 
-
+    @IBAction func touchedCard(sender: UIButton) {
+        if (self.frontShowing(sender)) {
+            self.showBack(sender)
+        } else {
+            self.showFront(sender)
+        }
+        self.flipCount++
+    }
+    
+    func frontShowing(button : UIButton) -> Bool {
+        let title : NSString? = button.currentTitle;
+        
+        return title?.length > 0;
+    }
+    
+    func showFront(button : UIButton) {
+        self.setImageAndTitleFor(button, imageName: "cardfront", title: "Ahearts")
+    }
+    
+    func showBack(button : UIButton) {
+        self.setImageAndTitleFor(button, imageName: "cardback", title: "")
+    }
+    
+    func setImageAndTitleFor(button: UIButton, imageName: String, title: String) {
+        button.setBackgroundImage(UIImage(named: imageName), forState: UIControlState.Normal)
+        button.setTitle(title, forState: UIControlState.Normal)
+    }
 }
 
