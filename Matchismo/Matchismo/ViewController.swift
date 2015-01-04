740588f2ce67a23232c07c31612bc463819bec5b
Jake Craige <jake@poeticsystems.com>
2015-01-04 10:25:25 -0600

    initial matchismo commit

---
+++ Matchismo/Matchismo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Matchismo
+//
+//  Created by Jake Craige on 1/4/15.
+//  Copyright (c) 2015 Jake Craige. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
