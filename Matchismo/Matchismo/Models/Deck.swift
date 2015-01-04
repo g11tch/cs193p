4b3f04d6710163f4675df330f568061d192ef782
Jake Craige <jake@poeticsystems.com>
2015-01-04 11:59:36 -0600

    add deck model

---
+++ Matchismo/Matchismo/Models/Deck.swift
@@ -0,0 +1,37 @@
+//
+//  Deck.swift
+//  Matchismo
+//
+//  Created by Jake Craige on 1/4/15.
+//  Copyright (c) 2015 Jake Craige. All rights reserved.
+//
+
+import Foundation
+
+class Deck : NSObject {
+    var cards : [Card] = []
+    
+    func addCard(card: Card, atTop: Bool) {
+        if atTop {
+            self.cards.insert(card, atIndex: 0)
+        } else {
+            self.cards.append(card)
+        }
+    }
+    
+    func addCard(card: Card) {
+        self.addCard(card, atTop: false)
+    }
+    
+    func drawRandomCard() -> Card? {
+        var randomCard : Card? = nil
+        
+        if self.cards.count > 0 {
+            let index = Int(arc4random()) % self.cards.count
+            randomCard = self.cards[index]
+            self.cards.removeAtIndex(index)
+        }
+        
+        return randomCard
+    }
+}
